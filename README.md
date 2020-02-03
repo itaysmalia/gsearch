b62f309713d3bf74ccde66dadb30b873781ebe01
itaysmalia <itays.malia@gmail.com>
2020-02-03 21:19:10 +0200

    added morw big sites and ascii art

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # gsearch
+   ____ _________  ____ ___________/ /_ 
+  / __ `/ ___/ _ \/ __ `/ ___/ ___/ __ \
+ / /_/ (__  )  __/ /_/ / /  / /__/ / / /
+ \__, /____/\___/\__,_/_/   \___/_/ /_/ 
+/____/                                  
+
+Made by
+
+  _____ _                 _____  __  __ 
+ |_   _| |               / ____||  \/  |
+   | | | |_ __ _ _   _  | (___  | \  / |
+   | | | __/ _` | | | |  \___ \ | |\/| |
+  _| |_| || (_| | |_| |  ____) || |  | |
+ |_____|\__\__,_|\__, | |_____(_)_|  |_|
+                  __/ |                 
+                 |___/                  
+
+
+usage: gsearch.py [-h] [-q QUERY [QUERY ...]] [-r RESULTS [RESULTS ...]]
+                  [-S BIGSITE [BIGSITE ...]]
+
+BIG Sites: 1 - stackoverflow.com, 2 - github.com, 3 - stackify.com, 4 -
+medium.com, 5 - quora.com
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -q QUERY [QUERY ...], --query QUERY [QUERY ...]
+                        search query
+  -r RESULTS [RESULTS ...], --results RESULTS [RESULTS ...]
+                        Open results by order (1 for first search result)
+  -S BIGSITE [BIGSITE ...], --bigsite BIGSITE [BIGSITE ...]
+                        Search ONLY from the specifics bigsites.
--- gsearch.py
+++ gsearch.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python3
-
+open_file = open
 import argparse
 from sys import argv
 from webbrowser import open,open_new_tab
 from requests import get
 from bs4 import BeautifulSoup as bs
 from threading import Thread
-bigsites={1:"stackoverflow.com",2:"github.com"}
+from time import sleep
+bigsites={1:"stackoverflow.com",2:"github.com",3:"stackify.com",4:"medium.com",5:"quora.com"}
 def main():
-    parser = argparse.ArgumentParser(description=f"Bigsites are: {' , '.join('{} - {}'.format(k,v) for k,v in bigsites.items())}")
-    parser.add_argument("-r", "--results", help="Open results by order (1 for first search result)",nargs='+')
+    parser = argparse.ArgumentParser(description="BIG Sites: "+', '.join('{} - {}'.format(k,v) for k,v in bigsites.items()))
     parser.add_argument("-q", "--query", help="search query",nargs='+')
-    parser.add_argument("-S", "--bigsite", 
-    help=f"Search ONLY from the specifics bigsites.")
+    parser.add_argument("-r", "--results", help="Open results by order (1 for first search result)",nargs='+')
+    parser.add_argument("-S", "--bigsite", nargs="+",help=f"Search ONLY from the specifics bigsites.")
     args = parser.parse_args()
-    
-    print("searched: {} opening results: {} from websites: {}".format(args.query,args.results,bigsites[int(args.bigsite)] if args.bigsite else "all"))
     if not args.query:
+        with open_file("./icon.txt","r") as file:
+            print(file.read())
+        sleep(0.5)
+        print()
+        print("Made by")
+        sleep(0.5)
+        print()
+        with open_file("./by.txt","r") as file:
+            print(file.read())
+        sleep(0.5)
         parser.print_help()
         return
+    print("searched: {} opening results: {} from websites: {}".format(args.query,args.results,[bigsites[int(x)] for x in args.bigsite] if args.bigsite else "all"))
     query = '+'.join(args.query)
     url="https://www.google.com/search?q="
     if args.bigsite:
-            query += f" site:{bigsites[int(args.bigsite)]}"
+            query += " |".join([f" site:{bigsites[int(x)]}" for x in args.bigsite])
     if not args.results:
         open(f"{url}{query}")
     else:
