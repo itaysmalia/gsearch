e74f7989932d4a2efcd8d0a34609df9ec3541d8a
itaysmalia <itays.malia@gmail.com>
2020-03-15 16:04:11 +0200

    pkging #1

--- gsearch.py
+++ gsearch.py
@@ -8,36 +8,66 @@ from bs4 import BeautifulSoup as bs
 from threading import Thread
 from time import sleep
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
-bigsites={1:"stackoverflow.com",2:"github.com",3:"stackify.com",4:"medium.com",5:"quora.com"}
+bigsites={1:"stackoverflow.com",2:"github.com",3:"stackify.com",4:"medium.com",5:"quora.com",6:"redis7.com"}
+
+def print_intro():
+    with open_file("./icon.txt","r") as file:
+        print(file.read())
+    sleep(0.5)
+    print()
+    print("Made by")
+    sleep(0.5)
+    print()
+    with open_file("./by.txt","r") as file:
+        print(file.read())
+    sleep(0.5)
+
 def main():
     parser = argparse.ArgumentParser(description="BIG Sites: "+', '.join('{} - {}'.format(k,v) for k,v in bigsites.items()))
-    parser.add_argument("-q", "--query", help="search query",nargs='+')
-    parser.add_argument("-r", "--results", help="Open results by order (1 for first search result)",nargs='+')
-    parser.add_argument("-s", "--site", nargs="+",help="Search ONLY from this sites.")
-    parser.add_argument("-S", "--bigsite", nargs="+",help="Search ONLY from the specifics bigsites.")
-    parser.add_argument("-l","--list",action='store_true',help="list n the first 10 search result, cant go with -r.")
+    parser.add_argument("-q",
+                         "--query",
+                        help="search query",
+                        nargs='+')
+    
+    parser.add_argument("-r",
+                        "--results",
+                        help="Open results by order (1 for first search result)",
+                        nargs='+')
+    
+    parser.add_argument("-s",
+                        "--site",
+                        nargs="+",
+                        help="Search ONLY from those sites.")
+    
+    parser.add_argument("-S",
+                        "--bigsite",
+                        nargs="+",
+                        help="Search ONLY from the specifics bigsites.")
+    
+    parser.add_argument("-l",
+                        "--list",
+                        action='store_true',
+                        help="list n the first 10 search result, cant go with -r.")
+    parser.add_argument("-i",
+                        "--insite",
+                        nargs="+",
+                        help="Search sites that contains this values")
     args = parser.parse_args()
+    
     if not args.query: #print intro and help
-        with open_file("./icon.txt","r") as file:
-            print(file.read())
-        sleep(0.5)
-        print()
-        print("Made by")
-        sleep(0.5)
-        print()
-        with open_file("./by.txt","r") as file:
-            print(file.read())
-        sleep(0.5)
+        print_intro()
         parser.print_help()
         return 
+    
     all_sites = []
-    all_sites += args.site if args.site else []
-    all_sites += args.bigsite if args.bigsite else []
+    all_sites += args.site or []
+    all_sites += args.bigsite or []
     print("searching {}{}{}{}".format(
         f"{' '.join(args.query)} ",
-        f"open results: {','.join(args.results)} " if args.results else "",
-        f"from websites: {', '.join(all_sites)} " if len(all_sites) > 0 else "",
+        f"open results: {','.join(args.results or '')} ",
+        f"from websites: {', '.join(all_sites or '')} ",
         "but listing first " if args.list else ""))
+    
     query = '+'.join(args.query)
     url="https://www.google.com/search?q="
     if args.bigsite:
@@ -47,7 +77,7 @@ def main():
     if not args.results:
         if args.list:
             #list
-            arr=get_filtered_a(f"{url}{query}")
+            arr=get_filtered_links(f"{url}{query}")
             for i,v in zip(range(1,11),arr[:10]):
                 print(f"{i}) {v.find('h3').text}")
             arr_of_indexes = list(map(int,input("=> ").split()))
@@ -65,13 +95,19 @@ def main():
         except:
             parser.print_help()
             return
-        filtered_arr = get_filtered_a(f"{url}{query}")
+        filtered_arr = get_filtered_links(f"{url}{query}")
         for i in args.results:
             open(filtered_arr[i-1]['href'],new=2,autoraise=False)
 
 
-def get_filtered_a(full_url):
-    soup = bs(get(full_url,headers=headers).content,'html.parser')
+def get_filtered_links(full_url):
+    soup = bs(
+        get(
+            full_url,
+            headers=headers
+            )
+            .content,'html.parser'
+        )
     arr = soup.find_all('a')
     links_array = [a for a in arr if a.has_attr('href')]
     return [x for x in links_array if x.find("h3")]
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gsearch-pkg-itaysmalia", # Replace with your own username
+    version="0.0.1",
+    author="Itay Sin Malia",
+    author_email="itays.malia@gmail.com",
+    description="A google cli interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/itaysmalia/gsearch.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
